f5fe4e42677e2261c42716043901b2d0fb789088
Owais Aslam <127122701+OwaisAslam911@users.noreply.github.com>
2024-09-28 17:04:40 +0500

    dart class task

---
+++ Dart Class Task/main.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+//owais aslam//
+class empInfo{
+    String? employeeName;
+    String? employeeDesignition;
+    double? employeeSalary;
+    double? employeeTax;
+    double? employeeTotalSalary
+    void displayEmpInfo(){
+        print("Employee Name is: $employeeName");
+        print("Employee DEsignition is: $employeeDesignition");
+        print("Employee salary is : $employeeSalary");
+        print("Tax deducted: $employeeTax");
+        print("Employee Total Salary is: $employeeTotalSalary");
+
+    }
+}
+void main(){
+print("Enter Your Name");
+String name = stdin.readLineSync() ?? "No Name Given";
+print("Enter Your Designition");
+String designition = stdin.readLineSync() ?? "No designition Given";
+print("Enter Your Salary");
+double  salary = double.parse(stdin.readLineSync() ?? "33000");
+
+double? tax;
+double? totalsalary;
+
+if(salary >= 100000 || salary <= 150000){
+  double tax = salary*10/100;
+   double totalsalary = salary - tax;
+}else{
+    print("you are safe from tax");
+}
+ empInfo employee = new empInfo();
+ employee.employeeName = name;
+ employee.employeeDesignition = designition;
+ employee.employeeSalary = salary;
+ employee.employeeTax = tax;
+ employee.employeeTotalSalary = totalsalary;
+employee.displayEmpInfo();
+}
